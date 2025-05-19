1b0aa982137fe544198d70a675c22d4f49729bbd
Ashutosh V M <ashutoshvmoff@gmail.com>
2025-05-20 00:15:42 +0530

    adjusted the webste position

--- app/pages/Home.tsx
+++ app/pages/Home.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useState, useEffect } from 'react';
 import  Hero  from "./hero";
 import { Navbar } from "./navbar";
 import { About } from "./about";
@@ -8,10 +11,36 @@ import { Contact } from "./contact";
 import Footer from "./footer";
 
 export default function Page() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    checkMobile();
+    window.addEventListener('resize', checkMobile);
+    return () => window.removeEventListener('resize', checkMobile);
+  }, []);
+
   return (
-    <main className="bg-black text-white font-sans" style={{ paddingTop: "80px" }}>
-        <Navbar />
-      <div style={{ marginTop: "2rem" }}>
+    <main 
+      className="bg-black text-white font-sans" 
+      style={{ 
+        paddingTop: "80px",
+        position: isMobile ? "relative" : "static",
+        margin: isMobile ? "0 auto" : "0",
+        width: "100%",
+        maxWidth: "100%",
+        overflow: "hidden"
+      }}>
+      <Navbar />
+      <div style={{ 
+        marginTop: "2rem",
+        position: isMobile ? "relative" : "static",
+        width: "100%",
+        maxWidth: isMobile ? "100vw" : "none",
+        padding: isMobile ? "0 1rem" : "0"
+      }}>
         <Hero />
       </div>
       <About />
