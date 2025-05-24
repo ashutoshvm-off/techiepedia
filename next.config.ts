578b3f0ae9d9859e6159c44ff6361fbf1400d4ef
Ashutosh V M <ashutoshvmoff@gmail.com>
2025-05-24 20:57:04 +0530

    rem

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: [
+    "http://192.168.56.1",
+    "http://localhost:3000",
+    "http://0.0.0.0:3000",
+  ],
 };
 
 export default nextConfig;
