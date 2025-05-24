f52e8ffe28417821f82e34da5efe591bad82af55
Ashutosh V M <ashutoshvmoff@gmail.com>
2025-05-24 21:16:43 +0530

    modified

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  allowedDevOrigins: [
-    "http://192.168.56.1",
-    "http://localhost:3000",
-    "http://0.0.0.0:3000",
-  ],
+  reactStrictMode: true,
+  headers: async () => {
+    return [
+      {
+        source: "/_next/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
