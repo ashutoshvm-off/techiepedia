7534abf5374e1358ebe9550b3387c43a621ac469
Aaron Vincent 6411 <aaronvincent6411@gmail.com>
2025-05-28 22:28:18 +0530

    crted navigation

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Home";
 import Eventsection from "./pages/event-section";
-import BlogSection from "./pages/blog-section";
 
 export default function App() {
   return (
@@ -11,7 +10,6 @@ export default function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/event" element={<Eventsection />} />
-          <Route path="/blog" element={<BlogSection />} />
         </Routes>
       </BrowserRouter>
     </main>
