b0186222f3d0e0d983016176370369cc27b7f713
kunjaimonai <yadukrishnanvs.official@gmail.com>
2025-06-03 02:17:22 +0530

    removed react dom and browser routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,9 @@
 "use client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Home";
-import Eventsection from "./pages/event-section";
-
 export default function App() {
   return (
     <main className="bg-black text-white font-sans">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/event" element={<Eventsection />} />
-        </Routes>
-      </BrowserRouter>
+      <HomePage />
     </main>
   );
 }
